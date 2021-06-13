8558bab4eb4dc2a7c790ed4c2bfa8c714f5230df
antonioluciofb <antonioluciofb@hotmail.com>
2021-06-13 13:43:38 -0300

    .

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Container, Text } from './style'
+
+const Header: React.FC = () => {
+  return (
+    <Container>
+      <Text>CRYPTODAY</Text>
+    </Container>
+  )
+}
+
+export default Header
